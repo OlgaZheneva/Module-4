70c531392066ed5bbdcb42f01556e0ff7b722b1c
Ольга Соколова-Жунёва <olgazhenevasokol@gmail.com>
2024-11-23 14:16:32 +0300

    Add files via upload

---
+++ Mod/calculateFallDistance.js
@@ -0,0 +1,7 @@
+function calculateFallDistance(t) {
+        
+        const g = 9.8;
+        const d = (1/2) * g * (t ** 2);
+        return d;       
+}
+export { calculateFallDistance };
---
+++ Mod/celsiusToFahrenheit.js
@@ -0,0 +1,7 @@
+function celsiusToFahrenheit(c) {
+  const w = 1.8; 
+  const b = 32; 
+  const f = (c * w) + b; 
+  return f;       
+}
+export {celsiusToFahrenheit};
